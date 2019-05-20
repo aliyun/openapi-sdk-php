ed0e0d067b46c8c9beede7081efc80815c3bbc04
顾咏 <zhijiang.nzj@alibaba-inc.com>
2019-05-20 18:31:32 +0800

    supported cms

--- src/Ecs/V20140526/EcsApiResolver.php
+++ src/Ecs/V20140526/EcsApiResolver.php
@@ -5,6 +5,7 @@ namespace AlibabaCloud\Ecs\V20140526;
 use AlibabaCloud\Client\Resolver\ApiResolver;
 
 /**
+ * @method ReportInstancesStatus reportInstancesStatus(array $options = [])
  * @method ModifyFleet modifyFleet(array $options = [])
  * @method DescribeFleets describeFleets(array $options = [])
  * @method DescribeFleetInstances describeFleetInstances(array $options = [])
@@ -282,6 +283,76 @@ class Rpc extends \AlibabaCloud\Client\Resolver\Rpc
 
     /** @var string */
     public $serviceCode = 'ecs';
+}
+
+/**
+ * @method string getReason()
+ * @method $this withReason($value)
+ * @method string getResourceOwnerId()
+ * @method $this withResourceOwnerId($value)
+ * @method string getDescription()
+ * @method $this withDescription($value)
+ * @method string getStartTime()
+ * @method $this withStartTime($value)
+ * @method array getDiskId()
+ * @method string getResourceOwnerAccount()
+ * @method $this withResourceOwnerAccount($value)
+ * @method string getOwnerAccount()
+ * @method $this withOwnerAccount($value)
+ * @method string getEndTime()
+ * @method $this withEndTime($value)
+ * @method string getOwnerId()
+ * @method $this withOwnerId($value)
+ * @method array getInstanceId()
+ * @method array getDevice()
+ */
+class ReportInstancesStatus extends Rpc
+{
+
+    /**
+     * @param array $diskId
+     *
+     * @return $this
+     */
+    public function withDiskId(array $diskId)
+    {
+        $this->data['DiskId'] = $diskId;
+        foreach ($diskId as $i => $iValue) {
+            $this->options['query']['DiskId.' . ($i + 1)] = $iValue;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array $instanceId
+     *
+     * @return $this
+     */
+    public function withInstanceId(array $instanceId)
+    {
+        $this->data['InstanceId'] = $instanceId;
+        foreach ($instanceId as $i => $iValue) {
+            $this->options['query']['InstanceId.' . ($i + 1)] = $iValue;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array $device
+     *
+     * @return $this
+     */
+    public function withDevice(array $device)
+    {
+        $this->data['Device'] = $device;
+        foreach ($device as $i => $iValue) {
+            $this->options['query']['Device.' . ($i + 1)] = $iValue;
+        }
+
+        return $this;
+    }
 }
 
 /**
