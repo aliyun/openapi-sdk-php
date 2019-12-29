96119fb933c0d9c62bba9a812ec0733a8c7336a5
sdk-team <sdk-team@alibabacloud.com>
2019-12-29 19:19:19 +0800

    Add new api.

--- src/Facebody/V20191230/FacebodyApiResolver.php
+++ src/Facebody/V20191230/FacebodyApiResolver.php
@@ -5,7 +5,9 @@ namespace AlibabaCloud\Facebody\V20191230;
 use AlibabaCloud\Client\Resolver\ApiResolver;
 
 /**
+ * @method CompareFace compareFace(array $options = [])
  * @method DetectFace detectFace(array $options = [])
+ * @method RecognizeFace recognizeFace(array $options = [])
  */
 class FacebodyApiResolver extends ApiResolver
 {
@@ -27,11 +29,135 @@ class Rpc extends \AlibabaCloud\Client\Resolver\Rpc
 }
 
 /**
+ * @method string getImageContentB()
+ * @method string getImageType()
+ * @method string getImageContentA()
+ * @method string getImageURLB()
+ * @method string getImageURLA()
+ */
+class CompareFace extends Rpc
+{
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withImageContentB($value)
+    {
+        $this->data['ImageContentB'] = $value;
+        $this->options['form_params']['ImageContentB'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withImageType($value)
+    {
+        $this->data['ImageType'] = $value;
+        $this->options['form_params']['ImageType'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withImageContentA($value)
+    {
+        $this->data['ImageContentA'] = $value;
+        $this->options['form_params']['ImageContentA'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withImageURLB($value)
+    {
+        $this->data['ImageURLB'] = $value;
+        $this->options['form_params']['ImageURLB'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withImageURLA($value)
+    {
+        $this->data['ImageURLA'] = $value;
+        $this->options['form_params']['ImageURLA'] = $value;
+
+        return $this;
+    }
+}
+
+/**
  * @method string getImageType()
  * @method string getImageURL()
  * @method string getImageContent()
  */
 class DetectFace extends Rpc
+{
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withImageType($value)
+    {
+        $this->data['ImageType'] = $value;
+        $this->options['form_params']['ImageType'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withImageURL($value)
+    {
+        $this->data['ImageURL'] = $value;
+        $this->options['form_params']['ImageURL'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function withImageContent($value)
+    {
+        $this->data['ImageContent'] = $value;
+        $this->options['form_params']['ImageContent'] = $value;
+
+        return $this;
+    }
+}
+
+/**
+ * @method string getImageType()
+ * @method string getImageURL()
+ * @method string getImageContent()
+ */
+class RecognizeFace extends Rpc
 {
 
     /**
--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.8.30';
+    const VERSION = '1.8.31';
 
     /**
      * @param Event $event
