acd01c56e0d55f56b78791346835fe81c097718b
sdk-team <sdk-team@alibabacloud.com>
2020-05-20 17:33:03 +0800

    Generated 2019-09-10 for waf-openapi.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 1.8.303 - 2020-5-20
+- Generated 2019-09-10 for `waf-openapi`.
+
+
 ## 1.8.302 - 2020-5-20
 - Edit GetTrace and SearchTraces api.
 
--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.8.302';
+    const VERSION = '1.8.303';
 
     /**
      * @param Event $event
