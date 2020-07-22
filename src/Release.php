ef62b32f5e005705f8145fd0ed75842dff73f2ce
sdk-team <sdk-team@alibabacloud.com>
2020-07-22 18:09:38 +0800

    Add source id for file uploader.

--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.8.388';
+    const VERSION = '1.8.389';
 
     /**
      * @param Event $event
--- src/Vcs/V20200515/VcsApiResolver.php
+++ src/Vcs/V20200515/VcsApiResolver.php
@@ -57,6 +57,9 @@ class Rpc extends \AlibabaCloud\Client\Resolver\Rpc
 
     /** @var string */
     public $method = 'POST';
+
+    /** @var string */
+    public $serviceCode = 'vcs';
 }
 
 /**
