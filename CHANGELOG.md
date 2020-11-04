2d6adfd446de88f42da39a55c1a8062a009f027d
sdk-team <sdk-team@alibabacloud.com>
2020-11-04 10:04:39 +0000

    Generated 2019-01-01 for Cassandra.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 1.8.583 - 2020-11-4
+- Generated 2019-01-01 for `Cassandra`.
+
+
 ## 1.8.582 - 2020-11-4
 - Support modifyInstnaceType.
 
--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.8.582';
+    const VERSION = '1.8.583';
 
     /**
      * @param Event $event
