babb8957199181ef7f16ae54479a33efac87f01d
sdk-team <sdk-team@alibabacloud.com>
2020-12-01 12:27:21 +0000

    Add task api.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 1.8.688 - 2020-12-1
+- Add task api.
+
+
 ## 1.8.687 - 2020-12-1
 - Add task api.
 
--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.8.687';
+    const VERSION = '1.8.688';
 
     /**
      * @param Event $event
