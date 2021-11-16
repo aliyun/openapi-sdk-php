342b5ab9121b78f9fad7ed9a6dcb7640827dcfac
sdk-team <sdk-team@alibabacloud.com>
2021-11-16 09:47:21 +0000

    Update apm interface parameter.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 1.8.1146 - 2021-11-16
+- Update apm interface parameter.
+
+
 ## 1.8.1145 - 2021-11-16
 - Update apm interface parameter.
 
--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.8.1145';
+    const VERSION = '1.8.1146';
 
     /**
      * @param Event $event
