aade4e7396cb59ef796ebaeffaf1437b63ccfed1
sdk-team <sdk-team@alibabacloud.com>
2022-01-21 08:37:57 +0000

    Add Event Rule sql filter.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 1.8.1239 - 2022-1-21
+- Add Event Rule sql filter.
+
+
 ## 1.8.1238 - 2022-1-21
 - Add Event Rule sql filter.
 
--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.8.1238';
+    const VERSION = '1.8.1239';
 
     /**
      * @param Event $event
