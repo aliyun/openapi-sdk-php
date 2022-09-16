123db3509ce89664f782590b516a167ca358d12d
sdk-team <sdk-team@alibabacloud.com>
2022-09-16 08:12:13 +0000

    Init amp.

--- src/AntiddosPublic/V20170518/AntiddosPublicApiResolver.php
+++ src/AntiddosPublic/V20170518/AntiddosPublicApiResolver.php
@@ -34,6 +34,9 @@ class Rpc extends \AlibabaCloud\Client\Resolver\Rpc
 
     /** @var string */
     public $method = 'POST';
+
+    /** @var string */
+    public $serviceCode = 'ddosbasic';
 }
 
 /**
