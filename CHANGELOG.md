aece618312cab5df02fdd945ed1ff1a1daad6a74
sdk-team <sdk-team@alibabacloud.com>
2022-09-21 08:09:47 +0000

    Generated 2016-11-01 for live.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 1.8.1490 - 2022-9-21
+- Generated 2016-11-01 for `live`.
+
+
 ## 1.8.1489 - 2022-9-21
 - Automatically generate sdk tasks.
 
--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.8.1489';
+    const VERSION = '1.8.1490';
 
     /**
      * @param Event $event
