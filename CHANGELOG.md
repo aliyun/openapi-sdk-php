55df06380fda2436562cdffdce67942204b4aa40
sdk-team <sdk-team@alibabacloud.com>
2025-03-21 10:26:40 +0000

    Generated 2019-03-07 for Cloudauth.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 1.8.2255 - 2025-3-21
+- Generated 2019-03-07 for `Cloudauth`.
+
+
 ## 1.8.2254 - 2025-3-21
 - Support Conditional Access Policy.
 
--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.8.2254';
+    const VERSION = '1.8.2255';
 
     /**
      * @param Event $event
